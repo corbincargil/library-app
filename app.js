38af3a52c9f7a7b1a811de8c08874e9ab9f2db3e
corbincargil <corbin.cargil@gmail.com>
2022-05-02 16:17:47 -0500

    added 'delete book button'.
    :wq

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ let myLibrary = [];
 let newBook;
 let addToColumn;
 
-//                                      Algo:
-// - Display 'new book' form when add book button clicked
-// - After user inputs info, get info when submit button is clicked
-// - Pass info from each input as an argument in addBookToLibrary 
-// - Add function to create a card for each book in the array
-// - Delete card button and ability to change progress status whenever to move the card to a different column
-// - Display each card on the HTML page
-
 //                                      To-do/Features:
 // - Optional inputs on the form (current page count, date finished, etc.)
 // - Delete book button
@@ -17,8 +9,6 @@ let addToColumn;
 // - media queries (mobile access)
 // - Image of book for background of the cards (different book/color for each book added?)
 // - Dark & Light themes
-
-
 
 //DOM elements
 const screenCover = document.querySelector('#screen-cover');
@@ -35,6 +25,7 @@ const notStartedColumn = document.querySelector('#not-started-column');
 const inProgressColumn = document.querySelector('#in-progress-column');
 const finishedColumn = document.querySelector('#finished-column');
 let newBookProgress = document.getElementsByName('progress');
+const updateProgressSubmitBtn = document.querySelector('#update-progress-submit-btn');
 
 
 //Book object
@@ -53,17 +44,7 @@ function Book(title, author, pages, bookProgress) {
 }
 
 //                                  Functions
-//add book to library
-const addBookToLibrary = (title, author, pages, bookProgress) => {
-    if (!title) {alert('Please input a title for the new book!')};
-    if (!author) {alert('Please input an author for the new book!')};
-    if (!pages) {alert('Please input a page count for the new book!')};
-    newBook = new Book(title,author,pages,bookProgress);
-    myLibrary.push(newBook);
-    return myLibrary;
-}
-
-//when 'add book' button is clicked, display form
+//display the form and blur the background by toggling on/off different classes
 const toggleForm = () => {
     formContainer.classList.toggle('hidden');
     formContainer.classList.toggle('showing');
@@ -73,7 +54,14 @@ const toggleForm = () => {
     pageTitle.classList.toggle('blur');
     addBookBtn.classList.toggle('blur');
 }
-
+//add book to library
+const addBookToLibrary = (title, author, pages, bookProgress) => {
+    if (!title) {alert('Please input a title for the new book!'); return;};
+    if (!author) {alert('Please input an author for the new book!'); return;};
+    if (!pages) {alert('Please input a page count for the new book!'); return;};
+    newBook = new Book(title,author,pages,bookProgress);
+    myLibrary.push(newBook);
+}
 //get radio btn values for newBookProgress
 const getNewBookProgress = () => {
     newBookProgress = document.getElementsByName('progress');
@@ -81,38 +69,70 @@ const getNewBookProgress = () => {
         if (newBookProgress[i].checked){ newBookProgress = newBookProgress[i].value}
     }
 }
+//remove all books from display
+const removeAllCards = () => {
+    let currentBooksDisplayed = document.querySelectorAll('.book-container');
+    currentBooksDisplayed.forEach(book => {
+        book.remove();
+    });
+}
+//remove book from library
+const removeBookFromLibrary = (btnClicked) => {
+    let bookNumberToRemove = btnClicked.target.id;
+    myLibrary.splice(bookNumberToRemove,1);
+    removeAllCards();
+    displayAllCards();
+}
 
-//create a book card and add it to the page
-const createCard = () => {
-    const newCardDiv = document.createElement('div');
-    let newCardTitle = document.createElement('p');
-    let newCardAuthor = document.createElement('p');
-    let newCardPageNumber = document.createElement('p');
-    newCardDiv.classList.add('book-container');
-    newCardTitle.classList.add('book-title');
-    newCardAuthor.classList.add('book-page-number');
-    newCardTitle.textContent = newBook.title;
-    newCardAuthor.textContent = newBook.author;
-    newCardPageNumber.textContent = newBook.pages;
-    newCardDiv.appendChild(newCardTitle);
-    newCardDiv.appendChild(newCardAuthor);
-    newCardDiv.appendChild(newCardPageNumber);
-    if (newBook.bookProgress == 3) {addToColumn = finishedColumn}
-    else if (newBook.bookProgress == 2) {addToColumn = inProgressColumn}
-    else {addToColumn = notStartedColumn}
-    addToColumn.appendChild(newCardDiv); 
+//displays all books in the myLibrary array according to their progress
+const displayAllCards = () => {
+    for (let i = 0; i < myLibrary.length; i++) {
+        newBook = myLibrary[i];
+        const newCardDiv = document.createElement('div');
+        let newCardTitle = document.createElement('p');
+        let newCardAuthor = document.createElement('p');
+        let newCardPageNumber = document.createElement('p');
+        let newCardBtnDiv = document.createElement('div');
+        let newCardProgressBtn = document.createElement('button');
+        let newCardDeleteBtn = document.createElement('button');
+        newCardDiv.classList.add('book-container',i);
+        newCardTitle.classList.add('book-title');
+        newCardAuthor.classList.add('book-author');
+        newCardPageNumber.classList.add('book-page-number');
+        newCardBtnDiv.classList.add('book-btn-div');
+        newCardProgressBtn.classList.add('update-progress-btn');
+        newCardDeleteBtn.classList.add('book-delete-btn');
+        newCardDeleteBtn.setAttribute('id',i);
+        newCardTitle.textContent = newBook.title;
+        newCardAuthor.textContent = newBook.author;
+        newCardPageNumber.textContent = newBook.pages;
+        newCardProgressBtn.textContent = 'Update Progress';
+        newCardDeleteBtn.textContent = 'Delete Book';
+        newCardDeleteBtn.addEventListener('click',removeBookFromLibrary);
+        newCardDiv.appendChild(newCardTitle);
+        newCardDiv.appendChild(newCardAuthor);
+        newCardDiv.appendChild(newCardPageNumber);
+        newCardDiv.appendChild(newCardBtnDiv);
+        newCardBtnDiv.appendChild(newCardProgressBtn);
+        newCardBtnDiv.appendChild(newCardDeleteBtn);
+        if (newBook.bookProgress == 3) {addToColumn = finishedColumn}
+        else if (newBook.bookProgress == 2) {addToColumn = inProgressColumn}
+        else {addToColumn = notStartedColumn}
+        addToColumn.appendChild(newCardDiv); 
+    }
 }
 
 //                                  Event Listeners
+addBookBtn.addEventListener('click',() => {
+    toggleForm();
+})
+
 submitBtn.addEventListener('click', () => {
     getNewBookProgress();
     addBookToLibrary(newTitle.value, newAuthor.value, newPageCount.value, newBookProgress)
     console.log(myLibrary);
-    createCard();
-    toggleForm();
-})
-
-addBookBtn.addEventListener('click',() => {
+    removeAllCards();
+    displayAllCards();
     toggleForm();
 })
 
@@ -120,8 +140,8 @@ formCloseBtn.addEventListener('click', () => {
     toggleForm();
 })
 
-
-
 //Example books
-addBookToLibrary('Mortification of Sin', 'John Owen', 250, false);
-addBookToLibrary('Mere Christianity', 'C.S. Lewis', 150, true);
+addBookToLibrary('Harry Potter and the Goblet of Fire', 'J.K. Rowling', 550,1);
+addBookToLibrary('Mistborn', 'Brandon Sanderson', 250,2);
+addBookToLibrary('Mere Christianity', 'C.S. Lewis', 150,3);
+displayAllCards();
